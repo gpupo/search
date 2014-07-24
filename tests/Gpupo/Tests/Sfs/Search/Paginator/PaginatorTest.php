3c92f648c1acbd95abced4566b404b362eb03be9
Gilmar Pupo <g@g1mr.com>
2014-07-24 18:12:26 -0300

    testes unitarios

---
+++ tests/Gpupo/Tests/Sfs/Search/Paginator/PaginatorTest.php
@@ -0,0 +1,235 @@
+<?php
+
+namespace Gpupo\Tests\Sfs\Search\Paginator;
+
+use Gpupo\Tests\Sfs\TestCaseAbstract;
+
+use Gpupo\Sfs\Search\Search;
+use Gpupo\Sfs\Search\Paginator\Paginator;
+use Gpupo\Sfs\Search\Query\Query;
+use Gpupo\Sfs\Search\Query\Keywords;
+
+class PaginatorTest extends TestCaseAbstract
+{
+    protected function getResultCollection($word)
+    {
+        $keywords = new Keywords;
+        $keywords->addKeyword($word);
+
+        $query = new Query($keywords);
+        $query->setIndex('produtoIndex');
+
+        $collection = Search::getInstance()->findByQuery($query);
+
+        return $collection;
+    }
+
+    public function testResultadosPossuiObjetoModeladoParaPaginacao()
+    {
+        $results = $this->getResultCollection('shampoo');
+        $this->assertInstanceOf('\Gpupo\Sfs\Search\Paginator\PaginableInterface', $results);
+        $this->assertInstanceOf('\Gpupo\Sfs\Search\Paginator\PaginatorInterface', $results->getPaginator());
+        $this->assertInstanceOf('\Gpupo\Sfs\Search\Paginator\Paginator', $results->getPaginator());
+
+        return $results;
+    }
+
+    /**
+     * @depends testResultadosPossuiObjetoModeladoParaPaginacao
+     */
+    public function testProcessaResultCollection($results)
+    {
+        $paginator = $results->getPaginator();
+        $paginator->paginateResult($results, 1, 10);
+        $this->assertEquals($results->getTotal(), $paginator->getTotalItemCount());
+
+    }
+
+    /**
+     *
+     * @dataProvider dataProviderOffset
+     */
+    public function testMarcaAPaginaAtual($page, $resultados, $limit)
+    {
+        $paginator = new Paginator;
+        $paginator->paginate($resultados, $page, $limit);
+        $this->assertEquals($page, $paginator->getCurrentPageNumber());
+    }
+    /**
+     *
+     * @dataProvider dataProviderOffset
+     */
+    public function testManipulaOffsetDeQuery($page, $resultados, $limit, $expectedValue)
+    {
+        $paginator = new Paginator;
+        $paginator->paginate($resultados, $page, $limit);
+        $this->assertEquals($expectedValue, $paginator->getOffset());
+    }
+
+    /**
+     *
+     * @dataProvider dataProviderOffset
+     */
+    public function testManipulaLimitDeQuery($page, $resultados, $limit)
+    {
+        $paginator = new Paginator;
+        $paginator->paginate($resultados, $page, $limit);
+        $this->assertEquals($limit, $paginator->getItemNumberPerPage());
+    }
+
+    /**
+     *
+     * @dataProvider dataProviderPaginas
+     */
+    public function testDivideResultadosEmPaginasDeAcordoComLimite($resultados,
+        $page, $limit, $expectedPages)
+    {
+        $paginator = new Paginator;
+        $this->assertInstanceOf('\Gpupo\Sfs\Search\Paginator\PaginatorInterface', $paginator);
+        $paginator->paginate($resultados, $page, $limit);
+
+        $this->assertEquals($expectedPages, $paginator->getPagesCount());
+    }
+
+    /**
+     *
+     * @dataProvider dataProviderRanges
+     */
+    public function testAcessoAoRangeDePaginasAproximadas($page, $resultados, $limit, array $rangeExpected)
+    {
+        $paginator = new Paginator;
+        $paginator->paginate($resultados, $page, $limit);
+
+        $data = $paginator->getPaginationData();
+
+        $this->assertEquals($data['pagesInRange'], $rangeExpected);
+    }
+
+    /**
+     *
+     * @dataProvider dataProviderCustomRanges
+     */
+    public function testPermiteCustomizacaoDoRangeDePaginasParaNavegacao($page, $resultados, $limit, $range, $rangeExpected)
+    {
+        $paginator = new Paginator;
+        $paginator->paginate($resultados, $page, $limit);
+        $paginator->setPageRange($range);
+
+        $this->assertEquals($range, $paginator->getPageRange());
+        $data = $paginator->getPaginationData();
+        $this->assertEquals($rangeExpected, $data['pagesInRange']);
+    }
+
+    /**
+     *
+     * @dataProvider dataProviderLongRanges
+     */
+    public function testAcessoAValoresDaPaginacao($page, $resultados, $limit)
+    {
+        $paginator = new Paginator;
+        $paginator->paginate($resultados, $page, $limit);
+
+        $list = array(
+            'last',
+            'current',
+            'numItemsPerPage',
+            'first',
+            'pageCount',
+            'totalCount',
+            'previous',
+            'next',
+            'pagesInRange',
+            'firstPageInRange',
+            'lastPageInRange',
+        );
+
+        foreach ($list as $k) {
+            $this->assertArrayHasKey($k, $paginator->getPaginationData());
+        }
+    }
+
+    public function dataProviderRanges()
+    {
+        $array = $this->dataProviderLongRanges();
+
+        $array[] = array(2, 20,  10, array(1,2));
+
+        return $array;
+    }
+
+    /**
+     *
+     * @return array [page, resultados, limit]
+     */
+    public function dataProviderLongRanges()
+    {
+        return array(
+
+            array(5, 100, 10, array(3,4,5,6,7)),
+            array(6, 100, 10, array(4,5,6,7,8)),
+            array(7, 100, 10, array(5,6,7,8,9)),
+            array(8, 100, 10, array(6,7,8,9,10)),
+        );
+    }
+
+    /**
+     *
+     * @return array [page, resultados, limit, $range, expectedRange]
+     */
+    public function dataProviderCustomRanges()
+    {
+        return array(
+            array(5, 100, 10, 3, array(4,5,6)),
+            array(5, 100, 10, 7, array(2,3,4,5,6,7,8)),
+            array(5, 100, 10, 9, array(1,2,3,4,5,6,7,8,9)),
+            array(5, 100, 10, 11, array(1,2,3,4,5,6,7,8,9,10)),
+            array(5, 100, 10, 21, array(1,2,3,4,5,6,7,8,9,10)),
+            array(5, 100, 2, 11, array(1,2,3,4,5,6,7,8,9,10,11)),
+            array(5, 100, 2, 13, array(1,2,3,4,5,6,7,8,9,10,11,12,13)),
+        );
+    }
+    /**
+     *
+     * @return array [resultados,page,limit,expectedPages]
+     */
+    public function dataProviderPaginas()
+    {
+        return array(
+            array(100, 1, 10, 10),
+            array(10, 1, 10, 1),
+            array(99, 1, 10, 10),
+            array(101, 1, 10, 11),
+            array(11, 1, 10, 2),
+            array(11, 1, 2, 6),
+        );
+    }
+
+    public function dataProviderOffset()
+    {
+        //page,results,limit,expectedValue
+        return array(
+            array(
+                3,10,2,4
+            ),
+            array(
+                3,100,10,20
+            ),
+            array(
+                4,100,10,30
+            ),
+            array(
+                5,100,10,40
+            ),
+            array(
+                9,99,10,80
+            ),
+            array(
+                9,99,1,8
+            ),
+            array(
+                99,99,1,98
+            ),
+        );
+    }
+
+}
---
+++ tests/Gpupo/Tests/Sfs/Search/SearchTest.php
@@ -0,0 +1,277 @@
+<?php
+
+namespace Gpupo\Tests\Sfs\Search;
+
+use Gpupo\Tests\Sfs\TestCaseAbstract;
+use Gpupo\Sfs\Search\Search;
+use Gpupo\Sfs\Search\Result\Collection;
+use Gpupo\Sfs\Search\Query\Query;
+use Gpupo\Sfs\Search\Query\Keywords;
+
+class SearchTest extends TestCaseAbstract
+{
+    /**
+     * @dataProvider dataProviderProdutosComMarcaNoNome
+     */
+    public function testResultadosContendoObjetosModelados($keyword)
+    {
+        $collection = new Collection;
+        $results = Search::getInstance()->getResultsByKeyword($keyword, $collection);
+        $this->assertInstanceOf('\Gpupo\Sfs\Search\Result\CollectionInterface', $results);
+
+        foreach ($results->toArray() as $item) {
+            $this->assertInstanceOf('\Gpupo\Sfs\Search\Result\ItemInterface', $item);
+        }
+    }
+
+    public function testPesquisaPalavraChaveSimples()
+    {
+         $results = Search::getInstance()->search(
+            'produtoIndex',
+            null,
+            array(array(
+                'key'    => '*',
+                 'values' => array(
+                    'shampoo',
+                 ),
+                'strict' => false,
+            )),
+            null,
+            20,
+            0
+        );
+
+         $this->assertInternalType('array', $results);
+
+         foreach ($results as $item) {
+             $this->assertArrayHasKey('attrs', $item);
+             $this->stringContainOneOrAlternatives(@json_encode($item),
+                'shampoo', 'condicionador');
+         }
+
+         return $results;
+    }
+
+    /**
+     *
+     * @depends testPesquisaPalavraChaveSimples
+     */
+    public function testPossuiQuantidadeLimiteDeResultados(array $results)
+    {
+
+       $this->assertGreaterThan(10, count($results));
+       $this->assertLessThanOrEqual(20, count($results));
+
+       return $results;
+    }
+
+    /**
+     * @depends testPossuiQuantidadeLimiteDeResultados
+     */
+    public function testResultadosPossuemAtributos(array $results)
+    {
+        foreach ($results as $item) {
+            $this->assertArrayHasKey('attrs' , $item);
+        }
+    }
+
+    public function testPesquisaPorMultiplasPalavras()
+    {
+        $results = Search::getInstance()->search(
+            'produtoIndex',
+            null,
+            array(array(
+                'key'    => '*',
+                 'values' => array(
+                    'shampoo',
+                    'condicionador',
+                 ),
+                'strict' => false,
+            )),
+            null,
+            20,
+            0
+        );
+
+         $this->assertInternalType('array', $results);
+
+         foreach ($results as $item) {
+            $this->assertArrayHasKey('attrs', $item);
+            $this->assertStringContainsOneOrMore(
+                array(
+                    'shampoo','condicionador', 'cabelo', 'tratamento'
+                ),
+                @json_encode($item)
+            );
+
+         }
+
+         return $results;
+    }
+
+    public function testPesquisaPorParteDePalavra()
+    {
+        $results = Search::getInstance()->search(
+            'produtoIndex',
+            null,
+            array(array(
+                'key'    => '*',
+                 'values' => array(
+                    'sham',
+                 ),
+                'strict' => false,
+            )),
+            null,
+            20,
+            0
+        );
+
+         foreach ($results as $item) {
+             $this->assertArrayHasKey('attrs', $item);
+             $this->assertStringContains('shampoo', @json_encode($item));
+         }
+
+         return $results;
+    }
+
+    public function testPesquisaComPalavrasForaDeOrdem()
+    {
+        $keywords = new Keywords;
+        $keywords->addKeyword('shampoo');
+        $keywords->addKeyword('condicionador');
+        $query = new Query($keywords);
+        $query->setIndex('produtoIndex');
+
+        $total['mode_1'] = Search::getInstance()->findByQuery($query)->getTotal();
+
+        //Mode 2
+        $keywords = new Keywords;
+        $keywords->readString('shampoo condicionador');
+        $query = new Query($keywords);
+        $query->setIndex('produtoIndex');
+
+        $total['mode_2'] = Search::getInstance()->findByQuery($query)->getTotal();
+
+        //Mode 3
+        $keywords = new Keywords;
+        $keywords->readString('condicionador shampoo');
+        $query = new Query($keywords);
+        $query->setIndex('produtoIndex');
+
+        $total['mode_3'] = Search::getInstance()->findByQuery($query)->getTotal();
+
+        $this->assertEquals($total['mode_1'], $total['mode_2']);
+        $this->assertEquals($total['mode_1'], $total['mode_3']);
+        $this->assertEquals($total['mode_2'], $total['mode_3']);
+    }
+
+    public function testAcessoAQuantidadeDeResultadosDisponiveis()
+    {
+        $results = Search::getInstance()->query(
+            'produtoIndex',
+            null,
+            array(array(
+                'key'    => '*',
+                 'values' => array(
+                    'shampoo',
+                 ),
+                'strict' => false,
+            )),
+            null,
+            20,
+            0
+        );
+
+        $this->assertGreaterThan(10, $results['total']);
+        $this->assertGreaterThan(10, $results['total_found']);
+
+        return $results;
+    }
+
+    /**
+     * @depends testAcessoAQuantidadeDeResultadosDisponiveis
+     */
+    public function testAcessoAQuantidadeDeResultadosDisponiveisPorPalavra($results)
+    {
+        $this->assertArrayHasKey('words', $results);
+
+        foreach ($results['words'] as $word) {
+            $this->assertArrayHasKey('docs', $word);
+            $this->assertArrayHasKey('hits', $word);
+        }
+    }
+
+    /**
+     * @cover \Gpupo\Sfs\Search\Result\Collection
+     */
+    public function testAcessoAResultadosEmObjetosModelados()
+    {
+        $collection = Search::getInstance()->getCollection(
+            'produtoIndex',
+            null,
+            array(array(
+                'key'    => '*',
+                 'values' => array(
+                    'shampoo'
+                 ),
+                'strict' => false,
+            )),
+            null,
+            20,
+            0
+        );
+
+        $this->assertInstanceOf('\Gpupo\Sfs\Search\Result\Collection', $collection);
+        $this->assertGreaterThan(10, $collection->getTotal());
+        $this->assertGreaterThan(10, $collection->getTotalFound());
+        $this->assertInternalType('integer', $collection->getTotal());
+        $this->assertInternalType('integer', $collection->getTotalFound());
+
+        return $collection;
+    }
+
+    public function dataProviderProdutosComMarcaNoNome()
+    {
+        return array(
+            array('herrera', array('carolina', '212', 'ch')),
+            array('azzaro', array('chrome', 'edt', 'edp', 'visit')),
+            array('lacoste',array('edt', 'edp')),
+            array('rabanne', array('calandre', 'black xs', 'million')),
+        );
+    }
+
+    public function testSuporteAMultiQueries()
+    {
+        $query = array();
+
+        $query[] = array(
+            'key' => '*',
+            'values' => array(
+                'shampoo',
+            ),
+            'strict' => false,
+        );
+
+        $query[] = array(
+            'key' => '*',
+            'values' => array(
+                'perfume',
+            ),
+            'strict' => false,
+        );
+
+        $multipleResults = Search::getInstance()->query(
+            'produtoIndex', null, $query, null, 2, 0
+        );
+
+        $this->assertCount(2, $multipleResults);
+
+        foreach ($multipleResults as $results) {
+            $this->assertArrayHasKey('total', $results);
+            $this->assertArrayHasKey('matches', $results);
+            foreach ($results['matches'] as $item) {
+                $this->assertArrayHasKey('attrs', $item);
+            }
+        }
+    }
+}
