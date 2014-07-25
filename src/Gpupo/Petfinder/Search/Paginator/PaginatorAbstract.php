b84e7c61472dbaea1577edce8da32a327c591389
Gilmar Pupo <g@g1mr.com>
2014-07-25 10:13:45 -0300

    rename sfs to petfinder

---
+++ src/Gpupo/Petfinder/Search/Paginator/PaginatorAbstract.php
@@ -0,0 +1,128 @@
+<?php
+/*
+ * This file is part of the sfs package.
+ *
+ * (c) Gilmar Pupo <g@g1mr.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Gpupo\Petfinder\Search\Paginator;
+
+/**
+ * Componente de paginacao de resultados da Busca
+ *
+ * Não gerencia os itens de resultados, mas apenas os números de limite e offset
+ * de uma query Sphinx Search
+ *
+ * Usado como referencia o Knp Pager component.
+ */
+abstract class PaginatorAbstract
+{
+
+    protected $range = 5;
+    protected $currentPageNumber;
+    protected $numItemsPerPage;
+    protected $totalCount;
+
+    public function setCurrentPageNumber($pageNumber)
+    {
+        $this->currentPageNumber = $pageNumber;
+    }
+
+    /**
+     * Get currently used page number
+     *
+     * @return integer
+     */
+    public function getCurrentPageNumber()
+    {
+        return $this->currentPageNumber;
+    }
+
+    public function setItemNumberPerPage($numItemsPerPage)
+    {
+        $this->numItemsPerPage = $numItemsPerPage;
+    }
+
+    /**
+     * Get number of items per page
+     *
+     * @return integer
+     */
+    public function getItemNumberPerPage()
+    {
+        return $this->numItemsPerPage;
+    }
+
+    public function setTotalItemCount($numTotal)
+    {
+        $this->totalCount = $numTotal;
+    }
+
+    /**
+     * Get total item number available
+     *
+     * @return integer
+     */
+    public function getTotalItemCount()
+    {
+        return $this->totalCount;
+    }
+
+    /**
+     * Offsets the result list by the number of places set by the count; 
+     * 
+     * This would be used for pagination through results, where if you have 20
+     * results per 'page', the second page would begin at offset 20, the third 
+     * page at offset 40, etc.
+     * 
+     * @return integer
+     */
+    public function getOffset()
+    {
+        return abs($this->getCurrentPageNumber() - 1) * $this->getItemNumberPerPage();
+    }
+
+    /**
+     * Processa os valors de resultado
+     * 
+     * @param integer $numTotal
+     * @param integer $page
+     * @param integer $limit
+     */
+    public function paginate($numTotal, $page, $limit = 10)
+    {
+        $this->setTotalItemCount($numTotal);
+        $this->setCurrentPageNumber($page);
+        $this->setItemNumberPerPage($limit);
+    }
+
+    public function getPagesCount()
+    {
+        return intval(ceil($this->getTotalItemCount() / $this->getItemNumberPerPage()));
+    }
+    
+    
+    /**
+     * Pagination page range
+     *
+     * @param integer $range
+     */
+    public function setPageRange($range)
+    {
+        $this->range = intval(abs($range));
+    }
+
+    /**
+     * Pagination page range
+     * 
+     * @return integer
+     */
+    public function getPageRange()
+    {
+        return $this->range;
+    }
+
+}
---
+++ src/Gpupo/Petfinder/Search/Query/FiltersAbstract.php
@@ -0,0 +1,144 @@
+<?php
+/*
+ * This file is part of the sfs package.
+ *
+ * (c) Gilmar Pupo <g@g1mr.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Gpupo\Petfinder\Search\Query;
+
+abstract class FiltersAbstract
+{
+    protected $list = array();
+
+    protected function add(array $array)
+    {
+        $this->list[$array['key']] = $array;
+
+        return $this;
+    }
+
+    protected function append($key, $value)
+    {
+        $this->list[$key]['values'][] = $value;
+
+        return $this;
+    }
+
+    /**
+     * Acesso aos valores de um filtro
+     *
+     * @param  string     $key
+     * @return array|null
+     */
+    public function getValues($key)
+    {
+        if (array_key_exists($key, $this->list)) {
+            return $this->list[$key]['values'];
+        }
+    }
+
+    /**
+     * Adiciona um valor a ValuesFilter existente
+     *
+     * @param  string $key
+     * @param  type   $value
+     * @return bool
+     */
+    public function appendValueFilter($key, $value)
+    {
+        if (!empty($value)) {
+            if ($this->getValues($key)) {
+                return $this->append($key, $value);
+            } else {
+                return $this->addValuesFilter($key, array($value));
+            }
+        } else {
+            //do nothing
+            return $this;
+        }
+    }
+
+    /**
+     * Filtra por Lista de valores de uma chave
+     *
+     * @param string $key    The key to filter on
+     * @param array  $values The values to be filtered
+     */
+    public function addValuesFilter($key, array $values)
+    {
+        $array = array(
+            'key'       => $key,
+            'values'    => $values,
+        );
+
+        return $this->add($array);
+    }
+    
+    /**
+     * Adiciona um filtro a partir de string no formato 0-10 (inicio - fim)
+     * 
+     * @param string $key
+     * @param string $string     
+     */
+    public function addStringRangeFilter($key, $string)
+    {
+        $array = explode('-', trim($string));
+        
+        $min = intval($array[0]);
+        if (empty($min)) {
+            $min = 1;
+        }
+        
+        $max = intval($array[1]);
+        if (empty($max)) {
+            $max = 9999;
+        }
+        
+        return $this->addRangeFilter($key, $min, $max);
+    }
+    
+
+    public function addRangeFilter($key, $min, $max)
+    {
+        $array = array(
+            'key'   => $key,
+            'min'   => $min,
+            'max'   => $max,
+        );
+
+        return $this->add($array);
+    }
+
+    public function addGreaterThanFilter($key, $int)
+    {
+        return $this->addRangeFilter($key, $int, 99999);
+    }
+
+    /**
+     * Sintaxe de retorno:
+     * <code>
+     *
+     * array(                                 // Filters only support integer values
+     *     array(
+     *         'key'    => 'some_search_key', // The key to filter on
+     *         'values' => array(30,...),     // The values to be filtered
+     *     ),
+     *     array(                             // This is a range filter
+     *         'key'    => 'some_search_key', // The key to filter on
+     *         'min'    => 5,                 // Min and Max value to filter between
+     *         'max'    => 105,
+     *     ),
+     *     ...
+     * );
+     * </code>
+     */
+    public function toArray()
+    {
+        return $this->list;
+    }
+
+}
